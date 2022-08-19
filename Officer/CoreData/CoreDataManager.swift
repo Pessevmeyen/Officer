95199665cb439a3e4055460ebff0b84a20d44115
Furkan <gitaristler.toplulugu@gmail.com>
2022-08-19 09:31:33 +0300

    refactor coreData
    Core Data Manager is made

--- Officer/CoreData/CoreDataManager.swift
+++ Officer/CoreData/CoreDataManager.swift
@@ -9,10 +9,94 @@ import Foundation
 import UIKit
 import CoreData
 
+protocol CoreDataManagerDelegate {
+    func saveToCoreData(id: Int, name: String, address: String, capacity: String, rooms: String, space: String, image: String)
+    func getFromCoreData(completion: @escaping ((Result<[Int], Error>) -> Void))
+    func deleteFromCoreData(officeId: Int)
+}
+
 
 class CoreDataManager {
     
+    func getFromCoreData(complation: @escaping ((Result<[Int], Error>) -> Void)) {
+        
+        var officesIdFromCoreData : [Int] = []
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Offices")
+        
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            for result in results as! [NSManagedObject] {
+                if let id = result.value(forKey: "id") as? Int {
+                    officesIdFromCoreData.append(id)
+                }
+            }
+            complation(.success(officesIdFromCoreData))
+        } catch {
+            complation(.failure(error))
+        }
+        
+    }
     
+    func saveToCoreData(id: Int, name: String, address: String, capacity: String, rooms: String, space: String, image: String) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let savedOffice = NSEntityDescription.insertNewObject(forEntityName: "Offices", into: context)
+        
+        savedOffice.setValue(id, forKey: "id")
+        savedOffice.setValue(name, forKey: "name")
+        savedOffice.setValue(address, forKey: "address")
+        savedOffice.setValue(capacity, forKey: "capacity")
+        savedOffice.setValue(rooms, forKey: "rooms")
+        savedOffice.setValue(space, forKey: "space")
+        savedOffice.setValue(image, forKey: "image")
+        
+        do {
+            try context.save()
+        } catch {
+            print("An Error Occured When Saving Data to Core Data!")
+        }
+        //Favoriler sayfasona notify ediyoruz değişiklik geldi uygula diye.
+        NotificationCenter.default.post(name: NSNotification.Name("veriGirildi"), object: nil)
+    }
+    
+    
+    func deleteFromCoreData(officeId: Int) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Offices")
+        
+        fetchRequest.predicate = NSPredicate(format: "id = %@", "\(officeId)")
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    if let id = result.value(forKey: "id") as? Int {
+                        if id == officeId {
+                            context.delete(result)
+                            
+                            do {
+                                try context.save()
+                            } catch {
+                                print("An Error Occured When Saving Deleted Data")
+                            }
+                            break
+                        }
+                    }
+                }
+            } else {
+                print("There nothing")
+            }
+            
+        } catch {
+            print("An Error Occured When Deleting Data From Core Data")
+        }
+    }
     
     
 }
